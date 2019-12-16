814a4b3011d8da9f50e17ff03ca01699fa62dd29
Ho Yin Keung <hykeung92@gmail.com>
2019-12-16 21:33:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
-    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    puts "  #{holiday.to_s.split("_").map{|i|i}.join(", ")
+    
   end
   end
 end
