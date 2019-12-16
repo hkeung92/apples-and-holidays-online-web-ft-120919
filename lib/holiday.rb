86a5377c5d08a5597f3127acb18473dcb4f0caa5
Ho Yin Keung <hykeung92@gmail.com>
2019-12-16 21:03:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,7 +45,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter].map{|holiday, supply| puts supply}
+  holiday_hash[:winter].map{|holiday, supply| supply}
 end
 
 def all_supplies_in_holidays(holiday_hash)
