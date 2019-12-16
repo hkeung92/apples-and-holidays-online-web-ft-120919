61ffd6ab3eadda7f0e20038378b3361c11fd37a0
Ho Yin Keung <hykeung92@gmail.com>
2019-12-16 21:09:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,8 +58,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday| 
+    puts season.capitalize
     season.each do |holiday, supply|
-      puts 
+      puts holiday
 end
 
 def all_holidays_with_bbq(holiday_hash)
